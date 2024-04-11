ab29bca61178e5fa1aec71b768064c9247a48c69
Cameron <github@cameronsquire.com>
2024-04-11 13:01:39 -0700

    cringe typo

--- ng/app/page.tsx
+++ ng/app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
 				<Card>
 					<CardBody>
 					<p id="intro">I know what you're thinking, <i>another nerd with too many
-                        domains</i> right? Hear me out for just a sec though, becuase I'm not just another software engineer.</p>
+                        domains</i> right? Hear me out for just a sec though, because I'm not just another software engineer.</p>
                 <p id="explain">I am a 24 year-old network engineering graduate, I like cars, security flaws, and public transit, I
                     enjoy messing with hardware and searching for vulnerabilities, both physical and virtual.</p>
                 <p id="pitch">If that's something you want on your team or just want to say hey, reach out and let's
