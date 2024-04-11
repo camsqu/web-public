2e02ab17fcb8457110f0c7ddda7d274850288db1
Cameron <github@cameronsquire.com>
2024-04-11 13:07:10 -0700

    apostrophies don't compile well

--- ng/app/page.tsx
+++ ng/app/page.tsx
@@ -30,17 +30,17 @@ export default function Home() {
 	return (
 		<section className="flex">
 			<div className="flex-col">
-				<h1 className={title()}>👋 Hey there, I'm <span className={title({ color: "cyan" })}>Cameron Squire</span></h1>
+				<h1 className={title()}>👋 Hey there, I&apos;m <span className={title({ color: "cyan" })}>Cameron Squire</span></h1>
 				<h2 className={`${subtitle()} animate__animated animate__fadeInUp`}>Networking Grad and HomeKit user</h2>
 				
 				<Spacer y={14} />
 				<Card>
 					<CardBody>
-					<p id="intro">I know what you're thinking, <i>another nerd with too many
-                        domains</i> right? Hear me out for just a sec though, because I'm not just another software engineer.</p>
+					<p id="intro">I know what you&apos;re thinking, <i>another nerd with too many
+                        domains</i> right? Hear me out for just a sec though, because I&apos;m not just another software engineer.</p>
                 <p id="explain">I am a 24 year-old network engineering graduate, I like cars, security flaws, and public transit, I
                     enjoy messing with hardware and searching for vulnerabilities, both physical and virtual.</p>
-                <p id="pitch">If that's something you want on your team or just want to say hey, reach out and let's
+                <p id="pitch">If that&apos;s something you want on your team or just want to say hey, reach out and let&apos;s
                     chat!</p>
 					</CardBody>
 				</Card>
